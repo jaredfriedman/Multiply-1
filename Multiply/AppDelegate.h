342adeaf8f4a5a32289116f6daffe82359990bc0
Yash Varma <vyash8@gmail.com>
2014-01-07 15:54:28 -0600

    first commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by Yash Varma on 1/7/14.
+//  Copyright (c) 2014 Yash Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Yash Varma on 1/7/14.
+//  Copyright (c) 2014 Yash Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
